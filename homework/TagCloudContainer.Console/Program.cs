86d2540f82f49c06ddf7705c1ecbc52896ff7ddb
ivanov.av <ivanov.av@skbkontur.ru>
2017-12-15 14:31:16 +0500

    Коммит несохраненных файлов

--- homework/TagCloudContainer.Console/Program.cs
+++ homework/TagCloudContainer.Console/Program.cs
@@ -8,20 +8,6 @@ using TagsCloudContainer.Interfaces;
 
 namespace TagCloudContainer.Console
 {
-    class Options
-    {
-        [ValueOption(0)]
-        public string InputFile{ get; set; }
-
-        [Option("o",DefaultValue = "words.png", HelpText = "Выходной файл")]
-        public string OutputFile { get; set; }
-
-        [Option("c", DefaultValue = 100, HelpText = "Количество слов")]
-        public int Top { get; set; }
-
-    }
-
-
     class Program
     {
         static void Main(string[] args)
